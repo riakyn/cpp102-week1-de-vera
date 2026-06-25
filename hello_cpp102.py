a66a4e60b2bdef937c404f65a66ad2763a697236
lei <eleonordevera2008@gmail.com>
2026-06-25 17:03:46 +0800

    Fix syntax error in print statement

--- hello_cpp102.py
+++ hello_cpp102.py
@@ -1,3 +1,3 @@
 print ("hello, CPP102!")
 print ("My name is Ma. Eleonor A. De Vera, Section 1CPE-A")
-print ("I am learning Python, Git, and Github.)
+print ("I am learning Python, Git, and Github.")
